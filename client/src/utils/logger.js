290c1141ba3eb8c126f99073c9c1fd2fcf05d37e
Milan Tej H D <milantej02@gmail.com>
2026-06-11 18:48:44 +0530

    Refactor sendLog function to simplify parameters

--- client/src/utils/logger.js
+++ client/src/utils/logger.js
@@ -1,28 +1,21 @@
 const LOGGER_URL = import.meta.env.VITE_LOGGER_URL;
 
-export async function sendLog(path, status = 200) {
-
-    try {
-
-        await fetch(`${LOGGER_URL}/log`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                site: window.location.hostname,
-                ip: "unknown",
-                path,
-                method: "GET",
-                status,
-                user_agent: navigator.userAgent
-            })
-        });
-
-    } catch (err) {
-
-        console.error(err);
-
-    }
-
+export async function sendLog(path) {
+  try {
+    await fetch(`${LOGGER_URL}/log`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        site: window.location.hostname,
+        path,
+        method: "GET",
+        status: 200,
+        user_agent: navigator.userAgent,
+      }),
+    });
+  } catch (err) {
+    console.error("Logger error:", err);
+  }
 }
